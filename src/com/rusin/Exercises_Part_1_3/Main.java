b333fff5b37f2b98616a628b8f6124649aa71c4c
PavelKh88 <rusin88888@gmail.com>
2021-11-21 23:16:23 +0200

    ooP

---
+++ src/com/rusin/Exercises_Part_1_3/Main.java
@@ -0,0 +1,23 @@
+package com.rusin.Exercises_Part_1_3;
+
+public class Main {
+    public static void main(String[] args) {
+    Phone Samsung = new Phone();
+    Samsung.setModel("Samsung Galaxy A 32 ");
+    Samsung.setMemory(128);
+    Samsung.setWeight(184);
+    Samsung.setAmountOfSimSlot(2);
+    Samsung.setScreenSize("6.4");
+    Samsung.setScreenResolution("2400 x 1080");
+    Samsung.setColor("Silver");
+    Samsung.setBackCameraPixel(64);
+    Samsung.setFrontCameraPixels(20);
+    Samsung.setMaxPhotoResolution("1920х1080");
+    Samsung.setMaxVideoResolution("1920х1080");
+    Samsung.setProcessorFrequency(2.0 + 1.8);
+    Samsung.setProcessorCore(2);
+    Samsung.setWiFiVersion("1.22.3");
+    Samsung.setGps(true);
+    Samsung.setPowerTypeConnector(true);
+    }
+}
---
+++ src/com/rusin/Exercises_Part_1_3/Phone.java
@@ -0,0 +1,101 @@
+package com.rusin.Exercises_Part_1_3;
+
+public class Phone {
+   //Model//
+    private String model;
+    public void setModel(String telModel){
+     model = telModel; }
+    public String getModel(){
+     return model; }
+   //Memory//
+    private int memory;
+    public void setMemory(int telMemory){
+     memory = telMemory; }
+    public int getMemory(){
+     return memory;}
+   //Weight//
+    private int weight;
+    public void setWeight(int telWeight){
+     weight = telWeight; }
+    public int getWeight(){
+     return weight; }
+   //SimSlot//
+    private int amountOfSimSlot;
+    public void setAmountOfSimSlot(int telSimSlot){
+     amountOfSimSlot = telSimSlot; }
+    public int getAmountOfSimSlot(){
+     return amountOfSimSlot; }
+   //ScreenSize//
+    private String screenSize;
+    public void setScreenSize(String telScreenSize){
+     screenSize = telScreenSize; }
+    public String getScreenSize(){
+     return screenSize; }
+  //ScreenResolution//
+    private String screenResolution;
+    public void setScreenResolution(String telScreenResolution){
+     screenResolution = telScreenResolution; }
+    public String getScreenResolution(){
+     return screenResolution; }
+  //Color//
+    private String color;
+    public void setColor(String telColor){
+     color = telColor; }
+    public String getColor(){
+     return color; }
+ //BackCameraPixels//
+    private int backCameraPixels;
+    public void setBackCameraPixel(int telBCP){
+     backCameraPixels = telBCP; }
+    public int getBackCameraPixels(){
+     return backCameraPixels; }
+ //FrontCameraPixels//
+    private int frontCameraPixels;
+    public void setFrontCameraPixels(int telFCP){
+     frontCameraPixels = telFCP; }
+    public int getFrontCameraPixels(){
+     return frontCameraPixels; }
+ //maxPhotoResolution//
+    private String maxPhotoResolution;
+    public void setMaxPhotoResolution(String telMPR){
+     maxPhotoResolution = telMPR; }
+    public String getMaxPhotoResolution(){
+     return maxPhotoResolution;
+    }
+ //maxVideoResolution//
+    private String maxVideoResolution;
+    public void setMaxVideoResolution(String telMVR){
+     maxVideoResolution = telMVR; }
+    public String getMaxVideoResolution(){
+     return maxVideoResolution; }
+ //processorFrequency//
+    private double processorFrequency;
+    public void setProcessorFrequency(double telPF){
+     processorFrequency = telPF; }
+    public double getProcessorFrequency(){
+     return processorFrequency; }
+ //processorCore//
+    private int processorCore;
+    public void setProcessorCore(int telPC){
+     processorCore = telPC;}
+    public int getProcessorCore(){
+     return processorCore; }
+ //WiFiVersion//
+    private String wiFiVersion;
+    public void setWiFiVersion(String telWiFi){
+     wiFiVersion = telWiFi; }
+    public String getWiFiVersion(){
+     return wiFiVersion; }
+ //GPS//
+    private boolean gps;
+    public void setGps(boolean telGPS){
+     gps = telGPS; }
+    public boolean getGps(){
+     return gps; }
+ //powerTypeConnector//
+    private boolean powerTypeConnector;
+    public void setPowerTypeConnector(boolean telPTC){
+     powerTypeConnector = telPTC; }
+    public boolean getPowerTypeConnector(){
+     return powerTypeConnector;}
+}
