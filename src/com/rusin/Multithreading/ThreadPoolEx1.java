8eec3a04919d56dac67045c7dc105fc3adad6e35
PavelKh88 <rusin88888@gmail.com>
2022-02-11 21:25:20 +0200

    Multithreaded

--- src/com/rusin/Multithreading/ThreadPoolEx1.java
+++ src/com/rusin/Multithreading/ThreadPoolEx1.java
@@ -3,14 +3,18 @@ package com.rusin.Multithreading;
 import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class ThreadPoolEx1 {
-    public static void main(String[] args) {
-        ExecutorService executorService = Executors.newFixedThreadPool(5);
+    public static void main(String[] args) throws InterruptedException {
+   //     ExecutorService executorService = Executors.newFixedThreadPool(5);
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
         for (int i = 0; i < 10 ; i++) {
             executorService.execute(new RunnableImpl100());
         }
-        System.out.println("Main ands");
+        executorService.shutdown();
+     //   executorService.awaitTermination(5, TimeUnit.SECONDS);
+      //  System.out.println("Main ands");
 
     }
 }
@@ -19,11 +23,12 @@ class RunnableImpl100 implements Runnable{
 
     @Override
     public void run() {
-        System.out.println(Thread.currentThread().getName());
+        System.out.println(Thread.currentThread().getName() + "Start");
         try {
-            Thread.sleep(500);
+            Thread.sleep(2000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+        System.out.println(Thread.currentThread().getName() + "And");
     }
 }
