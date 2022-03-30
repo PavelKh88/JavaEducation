91fd77ff1e4a74d8cf2d07f1f5a641fa2637aa70
PavelKh88 <rusin88888@gmail.com>
2022-03-30 12:04:51 +0300

    Work with Files

---
+++ src/work_with_files/CopyEx1.java
@@ -0,0 +1,24 @@
+package work_with_files;
+
+import java.io.*;
+
+public class CopyEx1 {
+    public static void main(String[] args) {
+        try(BufferedReader reader = new BufferedReader(new FileReader("text2.txt"));
+            BufferedWriter writer = new BufferedWriter(new FileWriter("text3.txt"))) {
+//            int character;
+//            while ((character = reader.read()) != -1) {
+//                writer.write(character);
+            String line;
+            while ((line = reader.readLine()) != null){
+                writer.write(line);
+                writer.write('\n');
+            }
+            System.out.println("Done!");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
